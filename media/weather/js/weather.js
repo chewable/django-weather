3bf098d1e86ea0bc078c439bf8575e71a5735eee
brian@brianpaulson.com <brian@brianpaulson.com@bd67d282-d34f-0410-a320-7bdd0abaab87>
2008-06-30 20:49:47 +0000

    Inital Commit

---
+++ media/weather/js/weather.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	 $(".toggle0").click(function () {
+	      $(".details0").slideToggle("slow");
+	    });
+  $(".toggle1").click(function () {
+    $(".details1").slideToggle("slow");
+  });
+	$(".toggle2").click(function () {
+    $(".details2").slideToggle("slow");
+  });
+	$(".toggle3").click(function () {
+    $(".details3").slideToggle("slow");
+  });
+	$(".toggle4").click(function () {
+    $(".details4").slideToggle("slow");
+  });
+	$(".toggle5").click(function () {
+    $(".details5").slideToggle("slow");
+  });
+	$(".toggle6").click(function () {
+    $(".details6").slideToggle("slow");
+  });
+	$(".toggle7").click(function () {
+    $(".details7").slideToggle("slow");
+  });
+});
